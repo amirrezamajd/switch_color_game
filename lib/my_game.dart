479a7edbd3c2d1df298024517bfbab9d75ce586f
amirrezamajd <amirrezamajd@yahoo.com>
2024-03-03 14:39:54 +0330

    first game

--- lib/my_game.dart
+++ lib/my_game.dart
@@ -1,26 +1,24 @@
-
 import 'package:flame/camera.dart';
 import 'package:flame/events.dart';
 import 'package:my_first_game/player.dart';
 import 'package:flame/game.dart';
 
-class MyGame extends FlameGame with TapCallbacks{
+class MyGame extends FlameGame with TapCallbacks {
   late Player myPlayer;
-  MyGame():super(
-    camera: CameraComponent.withFixedResolution(width: 600, height: 1000),
-  );
+  MyGame()
+      : super(
+          camera: CameraComponent.withFixedResolution(width: 600, height: 1000),
+        );
   @override
   void onMount() {
     debugMode = true;
-
     world.add(myPlayer = Player());
     super.onMount();
   }
+
   @override
   void onTapDown(TapDownEvent event) {
     super.onTapDown(event);
     myPlayer.jump();
   }
-
 }
-
